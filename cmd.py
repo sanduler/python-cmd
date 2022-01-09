96a8727d43bb5e34d28981aa1548ba03ee124360
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-08 17:28:55 -0800

    created the main and removal statemened

--- cmd.py
+++ cmd.py
@@ -1,3 +1,16 @@
 # Name: Ruben Sanduleac
 # Date: January 8th, 2022
 # Description: Simple CMD Line Application
+
+import random
+import string
+import os
+
+
+def main(initial_word, gen_word_count, res_file_name):
+    if os.path.exists(res_file_name):
+        print("Remove the existing file....")
+        os.removal(res_file_name)
+
+    n = 1000
+    for i in range()
